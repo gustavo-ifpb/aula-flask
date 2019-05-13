4ebe02931b48a48fd5f89c1959c2a6c1e3bfa89b
Gustavo Soares <gustavo@papaga.io>
2019-05-13 08:12:52 -0300

    flask: templates

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 def hello():
-    return 'hello world!'
-
-@app.route('/isaque')
-def isaque():
-    return 'Valeu isaque'
+    usuarioAutenticado = 'Gustavo'
+    nomes = ['isaque', 'milena', 'daniel']
+    return render_template(
+        'index.html', 
+        title='Desenvolvimento de Aplicações Web - INTIN 4',
+        alunos=nomes,
+        user=usuarioAutenticado)
