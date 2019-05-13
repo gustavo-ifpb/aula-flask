b2f4f9b9e4023522c344726dcac7d14d451fd646
Gustavo Soares <gustavo@papaga.io>
2019-05-13 09:00:58 -0300

    flask: request form

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, request
 
 from app import app
+
+posts = []
 
 @app.route('/')
 def hello():
@@ -11,3 +13,20 @@ def hello():
         title='Desenvolvimento de Aplicações Web - INTIN 4',
         alunos=nomes,
         user=usuarioAutenticado)
+
+@app.route('/form', methods=['POST', 'GET'])
+def helloForm():
+    if request.method == 'POST':
+        email = request.form['email']
+        if 'status' in request.form:
+            status = request.form['status']
+        else:
+            status = 'off'
+        msg = request.form['msg']
+        return render_template(
+            'form_view.html', 
+            email=email,
+            status=status,
+            message=msg)
+    else:
+        return 'Rota apenas para requisições do tipo POST'
